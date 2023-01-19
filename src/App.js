cac2c574478f8e23f7a7881266cd6640e516d05d
cjsgmail <cjs0097@gmail.com>
2023-01-19 17:15:37 +0900

    feat: Main Page Container

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { StorybookButton } from "./stories/Button.stories";
 import { StorybookModal } from "./stories/Modal.stories";
 import { StorybookTitle } from "./stories/Title.stories";
@@ -9,11 +10,52 @@ function App() {
         title={"스토리북으로 디자인 시스템 관리하기"}
         textColor={"grey"}
       />
-      <StorybookButton text={"확인"} size={"small"} color={"skyblue"} />
-      <StorybookButton text={"취소"} size={"big"} color={"skyblue"} />
-      <StorybookModal text={"확인하겠습니까?"} size={"big"} />
+      <Container>
+        <ComponentContainer>
+          <ComponentTitle>Button</ComponentTitle>
+          <StorybookButton text={"확인"} size={"smaller"} color={"skyblue"} />
+          <StorybookButton text={"확인"} size={"small"} color={"skyblue"} />
+          <StorybookButton text={"확인"} size={"big"} color={"skyblue"} />
+        </ComponentContainer>
+        <ComponentContainer>
+          <ComponentTitle>Modal</ComponentTitle>
+          <StorybookModal text={"확인하겠습니까?"} size={"big"} />
+        </ComponentContainer>
+      </Container>
     </>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: scroll;
+`;
+
+const ComponentContainer = styled.div`
+  position: relative;
+  width: 70%;
+  height: 400px;
+  border: 1px solid lightgray;
+  border-radius: 20px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ComponentTitle = styled.div`
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  height: 25px;
+  font-size: 20px;
+  background-color: white;
+  color: gray;
+  font-weight: bold;
+`;
